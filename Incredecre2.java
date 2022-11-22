2e61cb246c275ea10c07e00b68ac75b6203b67e0
Apu kumar Giri <87590236+apukumar212@users.noreply.github.com>
2022-11-22 19:34:03 +0530

    Add files via upload

---
+++ Incredecre2.java
@@ -0,0 +1,17 @@
+package com.task1;
+
+public class Incredecre2 
+{
+public static void main(String[] args) 
+{
+	int a=34;
+	int b=21;
+	int c=a++ + ++b;
+	int d=--a + --b + c--;
+	int e=a + +b + +c + d--;
+	int f=-a + b-- + -c - d++;
+	int sum=a+b+c+d+e+f;
+	System.out.println("sum="+sum);
+	
+}
+}
